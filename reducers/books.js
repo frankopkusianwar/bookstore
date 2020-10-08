d00e1e21fdc6c67c5b365ed7cfcc96e38ec02a26
frankopkusianwar <okirorfrank3@gmail.com>
2020-10-08 18:03:12 +0300

    Combine reducers

--- reducers/books.js
+++ reducers/books.js
@@ -3,7 +3,7 @@ const bookReducer = (state = [], actions) => {
     case CREATE_BOOK:
       return [...state, action.book]
     case REMOVE_BOOK:
-      return []
+      return [...state.filter(book => book.id !== action.book)]
     default:
       return state
   }
