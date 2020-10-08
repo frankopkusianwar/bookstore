ebfe4e1eb2380d00c5217862eec8cc7d8bbca48a
frankopkusianwar <okirorfrank3@gmail.com>
2020-10-08 18:13:44 +0300

    Add state to index file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './components/App';
+import rootReducer from '../reducers/index';
+
+const initialState = {
+  books: [
+    {
+      id: Math.floor(Math.random() * 100),
+      title: 'Ngugi Wa Thiongo',
+      category: 'Learning',
+    },
+    {
+      id: Math.floor(Math.random() * 100),
+      title: 'Biological Science',
+      category: 'Learning',
+    },
+    {
+      id: Math.floor(Math.random() * 100),
+      title: 'Romeo and Juliet',
+      category: 'Learning',
+    },
+  ],
+};
+
+const store = createStore(
+  rootReducer, initialState,
+);
 
 ReactDOM.render(
   <React.StrictMode>
